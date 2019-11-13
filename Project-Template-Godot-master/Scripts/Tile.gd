c697b77088ceb55dca39f2231b8a057d8cd57641
JLazarz <jlazarz@iu.edu>
2019-11-12 20:55:15 -0500

    Made it Juicy

---
+++ Project-Template-Godot-master/Scripts/Tile.gd
@@ -0,0 +1,11 @@
+extends StaticBody2D
+
+var gray = preload("res://Assets/tile_gray.png")
+var red = preload("res://Assets/tile_red.png")
+var blue = preload("res://Assets/tile_blue.png")
+var green = preload("res://Assets/tile_green.png")
+var purple = preload("res://Assets/tile_purple.png")
+
+onready var sprite = get_node("Sprite")
+var score = 10
+
